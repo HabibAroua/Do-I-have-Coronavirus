af5343406fa3ddadfa6e255fbc40f7bbfd378b2e
HabibAroua <habibha.aroua82@gmail.com>
2020-03-28 18:14:13 +0100

    add new android project

---
+++ app/src/main/java/com/example/do_i_have_coronavirus/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.do_i_have_coronavirus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
