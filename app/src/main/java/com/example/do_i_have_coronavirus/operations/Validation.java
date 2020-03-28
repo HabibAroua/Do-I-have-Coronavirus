e933780882ab4d5283c65725b7f9030491256518
HabibAroua <habibha.aroua82@gmail.com>
2020-03-28 19:04:50 +0100

    dev Validation class

--- app/src/main/java/com/example/do_i_have_coronavirus/operations/Validation.java
+++ app/src/main/java/com/example/do_i_have_coronavirus/operations/Validation.java
@@ -2,5 +2,18 @@ package com.example.do_i_have_coronavirus.operations;
 
 public class Validation
 {
+    private Calcul calcul;
+    private String resultOfTest;
+
+    public Validation(Calcul calcul)
+    {
+        this.calcul=calcul;
+    }
+
+    public String getResultOfTest()
+    {
+        return this.resultOfTest;
+    }
+
 
 }
